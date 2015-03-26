8309927dcacee7b26c833f2a311dba8fd9648d9b
EdwardReed <edwardareed@gmail.com>
2015-03-26 13:39:40 -0700

    Update Gulp Task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,8 +2,8 @@ var gulp       = require('gulp');
 var livereload = require('gulp-livereload');
 
 module.exports = function(){
-	// gulp.watch('src/javascript/**/*', ['browserify']);
+	gulp.watch('src/javascript/**/*', ['browserify']);
 	gulp.watch('src/less/**', ['less']);
 	gulp.watch('src/images/**', ['images']);
-	// livereload();
+	livereload();
 };
